996aaf72c377eab6b4205b4ee6e1414e8e5120cb
rahul yadav <Rahul_203022@saitm.org>
2024-02-14 15:23:07 +0530

    main

---
+++ main.js
@@ -0,0 +1,155 @@
+
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+
+    this.scrollY >= 50 ? header.classList.add('bg-header') 
+                       : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollY = window.pageYOffset
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
+
+
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
+
+sr.reveal(`.home__data, .footer__container, .footer__group`)
+sr.reveal(`.home__img`, {display: 700, origin: 'bottom'})
+sr.reveal(`.logos__img, .program__card, .pricing__card`,{interval: 100})
+sr.reveal(`.choose__img, .calculate__content`,{origin: 'left'})
+sr.reveal(`.choose__content, .calculate__img`,{origin: 'right'})
+
+const calculateForm = document.getElementById('calculate-form'),
+      calculateCm = document.getElementById('calculate-cm'),
+      calculateKg = document.getElementById('calculate-kg'),
+      calculateMessage = document.getElementById('calculate-message')
+
+const calculateBmi = (e) =>{
+    e.preventDefault()
+     
+    if(calculateCm.value === '' || calculateKg.value === ''){
+        
+        calculateMessage.classList.remove('color-green')
+        calculateMessage.classList.add('color-red')
+
+        calculateMessage.textContent = 'Fill in the Height and Weight'
+
+        setTimeout(() =>{
+             calculateMessage.textContent = ''
+        }, 3000)
+    } else{  
+        const cm = calculateCm.value / 100,
+              kg = calculateKg.value,
+              bmi = Math.round(kg / (cm * cm))
+        if(bmi < 18.5){
+            calculateMessage.classList.add('color-green')
+            calculateMessage.textContent = `Your BMI is ${bmi} and you are skinny`
+        } else if(bmi < 25){
+            calculateMessage.classList.add('color-green')
+            calculateMessage.textContent = `Your BMI is ${bmi} and you are healty`
+        }  else{
+            calculateMessage.classList.add('color-green')
+            calculateMessage.textContent = `Your BMI is ${bmi} and you are overweight`
+        } 
+
+        calculateCm.value = ''
+        calculateKg.value = ''
+
+        setTimeout(() =>{
+            calculateMessage.textContent = ''
+        }, 4000)
+    }         
+}
+calculateForm.addEventListener('submit', calculateBmi)
+
+
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message'),
+       contactUser = document.getElementById('contact-user')
+
+const sendEmail = (e) =>{
+    e.preventDefault()
+    if(contactUser.value === ''){
+        contactMessage.classList.remove('color-green')
+        contactMessage.classList.add('color-red')
+        contactMessage.textContent = 'You must enter your email'
+        setTimeout(() => {
+            contactMessage.textContent = ''
+        }, 3000)
+    } else{
+        emailjs.sendForm('service_l8hq9qb','template_0hlvo6n','#contact-form','ptilFB2YbkQm1X1jU')
+        .then(() =>{
+           contactMessage.classList.add('color-green')
+           contactMessage.textContent = 'You registered Successfully'
+
+
+           setTimeout(() =>{
+            contactMessage.textContent =''
+           }, 3000)
+        }, (error) =>{
+            
+            alert('OOPS! SOMETHING IS WRONG....', error)
+        })
+
+        
+        contactUser.value = ''
+    }
+}
+
+contactForm.addEventListener('submit',sendEmail)
